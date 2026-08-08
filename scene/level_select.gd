b858453cf3107144a6e97469815a98af5f5bd1b6
f-k-0-0-1 <khanfaiq264@gmail.com>
2026-08-08 16:32:00 +0530

    ui update

--- scene/level_select.gd
+++ scene/level_select.gd
@@ -3,13 +3,15 @@ extends Control
 @export var tab_normal: Texture2D
 @export var tab_selected: Texture2D
 
+@export var Level_Icon_normal: Texture2D
+@export var Level_Icon_Current: Texture2D
+
 @onready var title_label: Label = $RightPanel/Title_Ribbon/TitleLabel
 
 @onready var home_page: Control = $RightPanel/Panel/Content/HomePage
 @onready var cats_page: Control = $RightPanel/Panel/Content/CatsPage
 @onready var addons_page: Control = $RightPanel/Panel/Content/AddonsPage
 @onready var shop_page: Control = $RightPanel/Panel/Content/ShopPage
-
 
 @onready var tabs: Array[TextureButton] = [
 $RightPanel/Tabs/VBoxContainer/HomeTab,
@@ -18,10 +20,28 @@ $RightPanel/Tabs/VBoxContainer/AddonTab,
 $RightPanel/Tabs/VBoxContainer/ShopTab
 ]
 
+@onready var level_button_1: TextureButton = $RightPanel/Panel/Content/HomePage/ScrollContainer/MapContainer/LevelButton1
+@onready var level_button_2: TextureButton = $RightPanel/Panel/Content/HomePage/ScrollContainer/MapContainer/LevelButton2
+@onready var level_button_3: TextureButton = $RightPanel/Panel/Content/HomePage/ScrollContainer/MapContainer/LevelButton3
+@onready var level_button_4: TextureButton = $RightPanel/Panel/Content/HomePage/ScrollContainer/MapContainer/LevelButton4
+@onready var level_button_5: TextureButton = $RightPanel/Panel/Content/HomePage/ScrollContainer/MapContainer/LevelButton5
+@onready var level_button_6: TextureButton = $RightPanel/Panel/Content/HomePage/ScrollContainer/MapContainer/LevelButton6
+
+@onready var LevelIconTexure: Array[TextureButton] = [
+$RightPanel/Panel/Content/HomePage/ScrollContainer/MapContainer/LevelButton1,
+$RightPanel/Panel/Content/HomePage/ScrollContainer/MapContainer/LevelButton2,
+$RightPanel/Panel/Content/HomePage/ScrollContainer/MapContainer/LevelButton3,
+$RightPanel/Panel/Content/HomePage/ScrollContainer/MapContainer/LevelButton4,
+$RightPanel/Panel/Content/HomePage/ScrollContainer/MapContainer/LevelButton5,
+$RightPanel/Panel/Content/HomePage/ScrollContainer/MapContainer/LevelButton6
+]
+
 var current_tab := 0
+var current_level := 1
 
 func _ready() -> void:
 	select_tab(0)
+	select_level(1)
 	cats_page.visible = false
 	addons_page.visible = false
 	shop_page.visible = false
@@ -34,6 +54,15 @@ func select_tab(index: int) -> void:
 			tabs[i].texture_normal = tab_selected
 		else:
 			tabs[i].texture_normal = tab_normal
+
+func select_level(level: int) -> void:
+	current_level = level
+
+	for i in range(LevelIconTexure.size()):
+		if i == level - 1:
+			LevelIconTexure[i].texture_normal = Level_Icon_Current
+		else:
+			LevelIconTexure[i].texture_normal = Level_Icon_normal
 
 func _on_tab_1_pressed() -> void:
 	select_tab(0)
@@ -69,3 +98,22 @@ func _on_tab_4_pressed() -> void:
 
 func _on_back_button_pressed() -> void:
 	SceneManager.go_to_menu()
+
+
+func _on_level_button_1_pressed() -> void:
+	select_level(1)
+
+func _on_level_button_2_pressed() -> void:
+	select_level(2)
+
+func _on_level_button_3_pressed() -> void:
+	select_level(3)
+
+func _on_level_button_4_pressed() -> void:
+	select_level(4)
+
+func _on_level_button_5_pressed() -> void:
+	select_level(5)
+
+func _on_level_button_6_pressed() -> void:
+	select_level(6)
